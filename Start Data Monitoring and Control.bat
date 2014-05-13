e36161bf233a31cb7399f72d54915b0464aeeba9
Sol Simpson <sds-git@isolver-software.com>
2014-05-13 07:32:26 -0400

    Renaming commit (part 1)

--- Start Data Monitoring and Control.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-call %~dp0..\..\scripts\env.bat
-cd %~dp0\source\usermonitor\webserver\
-START "" python.exe start.py %*
--- Start DataCollection Service.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-call %~dp0..\..\scripts\env.bat
-cd %~dp0\source\usermonitor\datacollection\
-START "" python.exe start.py %*
-PAUSE
